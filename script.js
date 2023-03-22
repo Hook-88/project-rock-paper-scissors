d41e7cbbd3fb2195f226b8503952647599cfae9d
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-22 11:07:07 -0500

    Add psuedocode for getComputerChoice function

---
+++ script.js
@@ -0,0 +1,11 @@
+//create a string array variable and name it ROCKPAPERSCISSORS
+//set ROCKPAPERSCISSORS to the strings rock, paper, scissors
+//create a function that takes an array as arr
+//  create a number variable and name it randomNumber
+//  set randomNumber to a random flaoting number
+//  create a number variable and name is arrayLength
+//  set arrayLength to the length arr
+//  Set randomNumber to arrayLength * randomNumber
+//  set randomNumber to the closet downrounded interger
+//  return item from arr at index randomNumber
+// 
