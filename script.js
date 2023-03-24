b50dafde4d24f603ae025f2c809b722383aed0c1
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-24 11:07:23 -0500

    Add computerChoice algoritm

--- script.js
+++ script.js
@@ -2,7 +2,13 @@
 const WEAPONS = [];
 //push 'ROCK', 'PAPER', 'SCISSORS' to WEAPONS array
 WEAPONS.push('ROCK', 'PAPER', 'SCISSORS');
-console.log(WEAPONS);
-//create function computerChoice with an array as argument
-//generate random number between 0 and 2
-//return value if array at index of random number
+//create function computerChoice with weaponsArr as argument
+const computerChoice = (weaponsArr) => {
+  //generate random number between 0 and 2
+  //create let randomNumber
+  //set randomNumber to Math.random()
+  //set randomNumber to randomNumber * length of weaponsArr
+  //set randomNumber to Math.floot(randomNumber)
+  //return weaponsArr[randomNumber]
+}
+
