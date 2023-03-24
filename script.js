58cc0c5fa159d2cd499ee415c38625b41bf0e696
Paul Hoekjan <paulhoekjan@gmail.com>
2023-03-24 07:37:32 -0500

    Add function GetComputerChoice

--- script.js
+++ script.js
@@ -1,11 +1,19 @@
-//create a string array variable and name it ROCKPAPERSCISSORS
+//create a constant string array variable and name it ROCKPAPERSCISSORS
 //set ROCKPAPERSCISSORS to the strings rock, paper, scissors
-//create a function that takes an array as arr
-//  create a number variable and name it randomNumber
+const ROCKPAPERSCISSORS = ['Rock', 'Paper', 'Scissors'];
+//create function getComputerChoice that takes an array as argument
+const getComputerChoice = (arr) => {
+//  create a number variable randomNumber
 //  set randomNumber to a random flaoting number
+  let randomNumber = Math.random();
 //  create a number variable and name is arrayLength
 //  set arrayLength to the length arr
+  let arrayLength = arr.length;
 //  Set randomNumber to arrayLength * randomNumber
+  randomNumber *= arrayLength;
 //  set randomNumber to the closet downrounded interger
+  randomNumber = Math.floor(randomNumber);
 //  return item from arr at index randomNumber
+  return arr[randomNumber];
 // 
+}
