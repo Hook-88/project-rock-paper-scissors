dd7900556af35c90bcdae49065fbec4e75d7ffbf
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-03 15:47:10 -0500

    printMessage

--- script.js
+++ script.js
@@ -31,12 +31,17 @@ function playRound (e) {
   const computerChoice = getComputerChoice(WEAPONS);
   const playerChoice = e.target.value;
   if (roundIsATie(playerChoice, computerChoice)) {
-    printRoundResult("it's a tie");
+    printMessage("it's a tie");
   } else {
-    
+    if (playerIsRoundWinner(playerChoice, computerChoice)) {
+      printMessage(`You win ${playerChoice} beats ${computerChoice}`);
+    } else {
+      printMessage(`You lose ${computerChoice} beats ${playerChoice}`);
+    }
   }
+}
 
-} 
+
 
 function addClickEventToButtons () {
   const buttons = document.querySelectorAll('button');
@@ -49,11 +54,26 @@ function roundIsATie (choicePlayer, choiceComputer) {
   return choicePlayer === choiceComputer ? true : false;
 }
 
-function printRoundResult (message) {
+function printMessage (message) {
   let paragraph = document.createElement('p');
   paragraph.innerText = message;
   const roundsResultContainer = document.querySelector('#rounds-results-container');
   roundsResultContainer.appendChild(paragraph);
+}
+
+function playerIsRoundWinner (choicePlayer, choiceComputer) {
+  if (  (choicePlayer === 'ROCK' && choiceComputer === 'SCISSORS') ||
+        (choicePlayer === 'PAPER' && choiceComputer === 'ROCK') ||
+        (choicePlayer === 'SCISSORS' && choiceComputer === 'PAPER')
+  ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function updateScores () {
+
 }
 
 setUpGame();
