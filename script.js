1fe0d5eed2b1a95027a4d5e7868607759c522eae
Paul Hoekjan <paulhoekjan@gmail.com>
2023-04-03 14:57:07 -0500

    Add displayScore function

--- script.js
+++ script.js
@@ -1,59 +1,13 @@
-//Create WEAPONS array
 const WEAPONS = ['ROCK', 'PAPER', 'SCISSORS'];
-//create playerScore
+let playerScore = 0;
+let computerScore = 0;
 
-//Create function to play a game
-
-
-const computerChoice = arr => {
-  let randomNumber = Math.floor(Math.random() * arr.length);
-  return arr[randomNumber];
+function displayScores (scorePlayer, scoreComputer) {
+  const playerScorePlaceholder = document.querySelector('#score-player');
+  const computerScorePlaceholder = document.querySelector('#score-computer');
+  playerScorePlaceholder.innerHTML = scorePlayer;
+  computerScorePlaceholder.innerHTML = scoreComputer;
 }
 
-
-const setUpGame = (playerScore, computerScore) => {
-  //display the beginning scores
-  displayScores(playerScore, computerScore);
-  //create empty round results container and add to body
-  const roundResultsContainer = document.createElement('div');
-  body.appendChild(roundResultsContainer);
-}
-
-const displayScores = (playerScore, computerScore) => {
-  const body = document.querySelector('body');
-  const playerScoreElement = document.querySelector('#score-player');
-  const computerScoreElement = document.querySelector('#score-computer');
-  playerScoreElement.innerHTML = playerScore;
-  computerScoreElement.innerHTML = computerScore;
-}
-
-playGame();
-
-const playGame = () => {
-  setUpGame(playerScore, computerScore);
-  //create score var
-  let playerScore = 0;
-  let computerScore = 0;
-  //get computer choice from Weapons Array
-  console.log(computerChoice(WEAPONS));
-
-
-}
-
-
-
-const printMessage = (message) => {
-  if (typeof message !== 'string') {
-    console.log('arg is not a string, please enter a string value')
-    return false;
-  } else {
-    let body = document.querySelector('body');
-    let paragraph = document.createElement('p');
-    paragraph.innerHTML = message;
-    body.appendChild(paragraph);
-    return true;
-  }
-}
-
-
+displayScores(playerScore, computerScore);
 
